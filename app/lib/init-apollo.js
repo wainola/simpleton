eafd83c06902e01d07e4d7ddead3d7ddf3b9301f
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-01 09:09:02 -0300

    deleting client folder

--- app/lib/init-apollo.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient, InMemoryCache, HttpLink } from 'apollo-boost';
-import fetch from 'isomorphic-unfetch';
-
-const apolloClient = null;
-
-if (!process.browser) {
-  global.fetch = fetch;
-}
--- app/pages/index.js
+++
@@ -1,1 +0,0 @@
-export default () => <h1>Poto!</h1>;
