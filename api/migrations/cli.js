d295d48216d604144dd23133de043b7db6511744
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-04 22:42:53 -0300

    creating index to register the current migrations

--- api/migrations/cli.js
+++ api/migrations/cli.js
@@ -20,6 +20,8 @@ program.command('create <tableName> [fields...]').action((tableName, fields) => 
 
   const filename = `${date}_${tableName}`;
 
+  const urzaIndexPath = './api/migrations/registry/urza_index';
+
   const query = mappedFields.reduce((acc, item, idx, self) => {
     acc += `${item.join(' ')} ${(item[3] === 'PRIMARY KEY' && 'DEFAULT gen_random_uuid()') || ''}${
       self.length - 1 === idx ? '' : ','
@@ -38,6 +40,29 @@ program.command('create <tableName> [fields...]').action((tableName, fields) => 
     if (err) console.log('Some error::', err);
     console.log(`SUCCESS ON CREATING MIGRATION FILE FOR ${tableName.toUpperCase()} TABLE!`);
   });
+
+  const existsUrzaIndex = fs.existsSync(urzaIndexPath);
+
+  // CREATING URZA INDEX IF NOT EXISTS;
+  if (!existsUrzaIndex) {
+    fs.writeFile(urzaIndexPath, filename, { flag: 'wx' }, err => {
+      if (err) console.log('Some error creating the file', err);
+      console.log('CREATING THE URZA INDEX');
+      process.exit(1);
+    });
+  }
+
+  // WRITING TO THE URZA INDEX THE CURRENT FILE OF MIGRATIONS
+  fs.readFile(urzaIndexPath, 'utf8', (err, data) => {
+    if (err) console.log('Some error reading the Urza index', err);
+
+    const newData = `${data}\n${filename}`;
+    fs.writeFile(urzaIndexPath, newData, err => {
+      if (err) console.log('Error on writing file', err);
+    });
+  });
 });
+
+program.command('remove last').action(() => {});
 
 program.parse(process.argv);
--- api/migrations/registry/1551748428_user.js
+++
@@ -1,6 +0,0 @@
-
-  module.exports = {
-      tableDescriptor: 'user',
-      query: `CREATE TABLE user (ID UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid())`
-    }
-    
