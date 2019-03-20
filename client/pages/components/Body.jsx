97882d1326790c42c2cfacca24a331422b6aea53
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-19 23:14:35 -0300

    render childs

--- client/pages/components/Body.jsx
+++ client/pages/components/Body.jsx
@@ -1,11 +1,6 @@
+import React from 'react';
+
 export default props => {
   console.log('Body', props);
-  const [Header, MainContent, Footer] = props.children;
-  return (
-    <React.Fragment>
-      <div className="header-section">{Header}</div>
-      <div className="main-content-section">{MainContent}</div>
-      <div className="footer-section">{Footer}</div>
-    </React.Fragment>
-  );
+  return <React.Fragment>{props.children.map(item => item)}</React.Fragment>;
 };
