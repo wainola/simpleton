d7952c60c5723eb44c7db1a8452f54960946d490
Nicolás Riquelme <nrriquelme@gmail.com>
2019-03-30 15:11:37 -0300

    Input field tested

--- client/pages/components/Input/index.jsx
+++ client/pages/components/Input/index.jsx
@@ -1,36 +1,10 @@
 import React from 'react';
 
-export const Input = ({
-  title,
-  placeholder,
-  type,
-  handleChange,
-  className,
-  descriptor,
-  CustomAlert,
-  validData
-}) => {
-  console.log('validData', validData);
-
-  let value;
-
-  if (value !== undefined) {
-    value = validData.value;
-  }
-
-  return (
-    <React.Fragment>
-      <h5>{title}</h5>
-      <input
-        type={type}
-        placeholder={placeholder}
-        onChange={handleChange}
-        className={className}
-        data-descriptor={descriptor.toLowerCase()}
-      />
-      {value !== undefined && value !== true && <CustomAlert validation={{ value, descriptor }} />}
-    </React.Fragment>
-  );
-};
+export const Input = ({ type, title, name, placeholder, handleChange = undefined }) => (
+  <React.Fragment>
+    <span className="input-title">{title}</span>
+    <input type={type} name={name} placeholder={placeholder} onChange={handleChange} />
+  </React.Fragment>
+);
 
 export default Input;
