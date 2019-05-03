db85069173dfd3ed5566e5772cc0955597499f44
Nicolás Riquelme <nrriquelme@gmail.com>
2019-05-02 23:29:17 -0400

    Update dependencies

--- client/pages/Home.jsx
+++ client/pages/Home.jsx
@@ -4,6 +4,7 @@ import Body from './components/Body';
 import Header from './components/Header/Header';
 import MainForm from './components/MainForm';
 import Description from './components/Description';
+import Navbar from './components/NavBar';
 import Head from './Head';
 import './styles.scss';
 
@@ -16,18 +17,37 @@ console.log('GET CONFIG', getConfig());
 export default ({ items, formFields }) => (
   <React.Fragment>
     <Head />
+    {/* <Container>
+      <Row>
+        <Navbar />
+      </Row>
+    </Container> */}
     <Container fluid>
       <Header />
-    </Container>
-    <Container>
-      <Row>
-        <Description />
-      </Row>
     </Container>
     <Container fluid>
       <Row>
         <MainForm />
       </Row>
     </Container>
+    <Container>
+      <Row>
+        <Description />
+      </Row>
+    </Container>
   </React.Fragment>
 );
+
+
+DIVORCIOS
+DIVORCIO MUTUO ACUERDO
+DIVORCIOS UNILATERALES
+
+SEPARACION JUDIFICIAL
+LUQUIDACION DE SOCIEDAD CONYUGAL
+
+REGULACION DE ALIMENTOS
+RELACION DIRECTA Y REGULAR
+CUIDADO PERSONAL
+
+TRANSACCIONES
