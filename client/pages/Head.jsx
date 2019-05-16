14161a0ad522ad1375b55b9eadcaf66d46ebbd08
Nicolás Riquelme <nrriquelme@gmail.com>
2019-05-15 23:36:35 -0400

    Change title of the page

--- client/pages/Head.jsx
+++ client/pages/Head.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default () => (
   <React.Fragment>
     <Head>
-      <title>Simpleton Site!</title>
+      <title>Natalia Scheuer Abogada</title>
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"
